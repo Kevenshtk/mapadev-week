46b57a99ef21e2eecbbb292e97f70841ed992b5c
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2023-03-03 18:15:43 -0300

    update index.js e .html
    Finalizado o js

--- projeto-marvel/src/js/index.js
+++ projeto-marvel/src/js/index.js
@@ -1,7 +1,7 @@
-
-    const personagens = document.querySelectorAll('.personagem');
+const personagens = document.querySelectorAll('.personagem');
 
     personagens.forEach((personagem) => {
+
         personagem.addEventListener('mouseenter', () =>{
             
             const idSelecionado = personagem.attributes.id.value;
@@ -21,30 +21,3 @@
     
         })
     })
-
-       function jogador2(){
-
-                const personagens = document.querySelectorAll('.personagem');
-
-                personagens.forEach((personagem) => {
-                    personagem.addEventListener('mouseenter', () =>{
-            
-                    const idSelecionado = personagem.attributes.id.value;
-
-                    const personagemSelecionado = document.querySelector('.selecionado');
-                    personagemSelecionado.classList.remove('selecionado');
-
-                    personagem.classList.add('selecionado');
-
-                    const imagemJogador2 = document.getElementById('personagem-jogador-2');
-                    imagemJogador2.src = `./src/img/${idSelecionado}.png`;
-            
-                    const nomejogador2 = document.getElementById('nome-jogador-2');
-                    const nomeSelecionado = personagem.getAttribute('data-name');
-            
-                    nomejogador2.innerHTML = nomeSelecionado;
-
-                })
-            })
-
-        }
